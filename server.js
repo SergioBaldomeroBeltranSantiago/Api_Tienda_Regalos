5be065cd315322d225690578d9cfc5ec9b8ec4fa
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2023-12-02 21:37:45 -0700

    Añadidos todos los routes para el servicio

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ require("dotenv").config();
 
 const errorHandler = require("./errorHandler.js");
 const sequelize = require("./Database/db.js");
+const GestorInventario = require("./Routes/GestorInventario.js");
+const GestorProducto = require("./Routes/GestorProducto.js");
+const GestorPromocion = require("./Routes/GestorInventario.js");
+const GestorVenta = require("./Routes/GestorVenta.js");
 
 //Utilidades
 const app = express();
 app.use(cors());
 app.use(errorHandler);
 const port = process.env.API_PORT || 3000;
+
+//Routing
+app.use("/inventario", GestorInventario);
+app.use("/producto", GestorProducto);
+app.use("/promocion", GestorPromocion);
+app.use("/venta", GestorVenta);
 
 //Peticiones
 app.get("/", (req, res, next) => {

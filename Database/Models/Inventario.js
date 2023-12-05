83f7ff7d62e2a0d08defa070cc193ce8d5175b58
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2023-12-05 14:24:49 -0700

    Eliminada la fecha, ya no es necesario

--- Database/Models/Inventario.js
+++ Database/Models/Inventario.js
@@ -1,39 +1,35 @@
-//Dependencias
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../db.js");
+// Dependencias
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db.js');
 
 class Inventario extends Model {}
 Inventario.init(
-  {
-    ID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        'ID': {
+            'type': DataTypes.INTEGER,
+            'allowNull': false,
+            'primaryKey': true,
+            'autoIncrement': true
+        },
+        'Nombre': {
+            'type': DataTypes.TEXT,
+            'allowNull': false
+        },
+        'Descripcion': {
+            'type': DataTypes.TEXT,
+            'allowNull': false
+        },
+        'CantidadActual': {
+            'type': DataTypes.INTEGER,
+            'allowNull': false
+        }
     },
-    Nombre: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    Descripcion: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    CantidadActual: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    FechaAdquision: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Inventario",
-    tableName: "Inventario",
-    timestamps: false,
-  },
+    {
+        sequelize,
+        'modelName': 'Inventario',
+        'tableName': 'Inventario',
+        'timestamps': false
+    }
 );
 
 module.exports = Inventario;
